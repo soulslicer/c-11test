45f871fd0dcaba9f24d7afb40f13fb31b6381830
Raaj <yaadhavraaj@gmail.com>
2017-08-14 22:24:48 +0800

    todo

--- c++11test.cpp
+++ c++11test.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
-int
-main (int argc, char ** argv)
+
+//range-based for loops
+//lambda expressions
+//auto
+//constexpr
+//explicit conversion functions
+//std::thread, std::atomic
+//move constructors std::move and the && operator
+//nullptr
+//static assert
+//variadic templates
+//initializing non-const member variables in the header file
+//traits //https://github.com/soulslicer/OctreeOCL/blob/master/src/Octree.hpp
+
+// https://www.toptal.com/c-plus-plus/interview-questions
+// https://tests4geeks.com/cpp-interview-questions/
+// http://thispointer.com/c11-tutorial/
+// http://www.thegeekstuff.com/2016/02/c-plus-plus-11/comment-page-1/
+
+int main (int argc, char ** argv)
 {
-  return (0);
+    struct x{
+        int m;
+    };
+
+    return (0);
 }
